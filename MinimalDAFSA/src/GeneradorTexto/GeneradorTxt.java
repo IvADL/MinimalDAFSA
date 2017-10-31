02dd21360215f6154ddc96d7da1cd9ce4d1da9db
danilocr <crucesdanilo@gmail.com>
2017-10-31 11:24:45 -0500

    GeneradorTxt Genera cadenas de letras sin repetirse

--- MinimalDAFSA/src/GeneradorTexto/GeneradorTxt.java
+++ MinimalDAFSA/src/GeneradorTexto/GeneradorTxt.java
@@ -4,9 +4,32 @@ public class GeneradorTxt {
 	public static void main(String[] args){
 		//Letras Ascii 97=a 98=b 99=c ..... 122=z
 		int letini=97;
-		int letfin=122;
-		
-		int letraAle= (int)(Math.random()*(letini-letfin)+letini);
-		System.out.println((char)letraAle);
+		int letfin=100;
+		int npalabras=10;
+		int longitudPalabra=5;
+		String[] diccionario=new String[npalabras];
+		for(int i=0;i<npalabras;i++){
+			int longiPal=(int)(Math.random()*(longitudPalabra)+1);
+			String palabra="";
+			for(int j=0;j<longiPal;j++){
+				int letraAle= (int)Math.floor(Math.random()*(letfin-letini)+letini);
+				palabra=palabra+(char)letraAle;
+			}
+			if(estaPalabra(palabra,diccionario,i))
+				i--;
+			else
+				diccionario[i]=palabra;
+		}
+		for(String a:diccionario)
+			System.out.println(a);
+	}
+	public static boolean estaPalabra(String pal,String[] dic,int j){
+		boolean esta=false;
+		int i=0;
+		while( i<j && !esta){
+			esta=(dic[i].equals(pal))?true:false;
+			i++;
+		}		
+		return esta;
 	}
 }
