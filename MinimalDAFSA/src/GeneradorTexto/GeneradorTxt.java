0eb3d81eca02474df9d45e53bc57b70a006efa31
danilocr <crucesdanilo@gmail.com>
2017-11-02 10:46:56 -0500

    No es necesario ordenar las letras solo por longitud

--- MinimalDAFSA/src/GeneradorTexto/GeneradorTxt.java
+++ MinimalDAFSA/src/GeneradorTexto/GeneradorTxt.java
@@ -9,8 +9,10 @@ public class GeneradorTxt {
 	public GeneradorTxt(int li,int lf,int np,int lp){
 		generarArchivoTxt(li,lf,np,lp);
 	}
+	public GeneradorTxt(){}
 	public static void main(String[] args){
-		new GeneradorTxt(97,100,5,5);
+	
+		new GeneradorTxt().generarArchivoTxt(97, 100, 5, 5);
 	}
 	//Genera un archivo con texto aleatorios
 	public void generarArchivoTxt(int li,int lf,int np, int lp){
@@ -35,7 +37,7 @@ public class GeneradorTxt {
 		texto(diccionario,longitudPalabra);
 	}
 	//Verifica si pal esta en el array dic con un j de tope de palabras
-	public static boolean estaPalabra(String pal,String[] dic,int j){
+	private boolean estaPalabra(String pal,String[] dic,int j){
 		boolean esta=false;
 		int i=0;
 		while( i<j && !esta){
@@ -46,51 +48,20 @@ public class GeneradorTxt {
 	}
 	//Ordena las palabras por longitud y luego usando compare to
 	//Luego crea el archivo txt
-	public static void texto(String[] A,int n){
-		String[] merge=new String[A.length];
-		int p=0;
-		for(int i=n;i>0;i--){
-			int can=cantidad(A,i);
-			String[] aux=new String[can];
-			for(int j=0,k=0;j<A.length && k<can;j++)
-				if(A[j].length()==i){
-					aux[k]=(A[j]);k++;
-					}
-			ordenarTexto(aux);
-			System.arraycopy(aux, 0, merge, p, aux.length);
-			p=p+can;
-		}
-		//Se crea el archivo txt
+	private void texto(String[] A,int n){
 		File archivo=new File("texto.txt");
 		BufferedWriter escribir;
 		try {
 			escribir = new BufferedWriter(new FileWriter(archivo));
-			for(String m:merge)
-				escribir.write(m+"\r\n");
+		for(int i=n;i>0;i--){
+			for(int j=0;j<A.length;j++)
+				if(A[j].length()==i)
+					escribir.write(A[j]+"\r\n");
+		}
 			escribir.close();
 		} catch (IOException e) {
 			System.out.println("ERROR : " + e) ;
 		}
-	}
-	//Señala la cantidad de apariciones en el arreglo A con longitud n
-	public static int cantidad(String[] A, int n){
-		int c=0;
-		for(int i=0;i<A.length;i++)
-			if(A[i].length()==n)
-				c++;
-		return c;
-	}
-	//Ordena usando compare to
-	public static void ordenarTexto(String[] A){
-		for (int i = 0; i < A.length; i++) {
-			String aux = A[i];
-	    	int j = i;
-	    	while (j > 0 && aux.compareTo(A[j-1])<0) {
-	    		A[j] = A[j-1];
-	    	    j--;
-	    	}
-	    	A[j] = aux;
-	    }
 	}
 	
 }
